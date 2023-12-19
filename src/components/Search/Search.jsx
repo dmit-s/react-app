758064c16965bcfb9335024506037302d45224cf
dmit-s <dwhatly@gmail.com>
2023-12-19 14:07:25 +0300

    upd

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,14 +1,34 @@
 import SvgIcon from "../SvgIcon/SvgIcon";
 import styles from "./Search.module.scss";
 
-const Search = ({ onSubmit, className, placeholder, searchValue, handleChange}) => {
+const Search = ({
+  onSubmit,
+  className,
+  placeholder,
+  searchValue,
+  handleChange,
+}) => {
   return (
-    <form onSubmit={onSubmit} className={`${styles.wrapper} ${className || ""}`}>
+    <form
+      onSubmit={onSubmit}
+      className={`${styles.wrapper} ${className || ""}`}
+    >
       <SvgIcon iconName="search" svgClass={styles.searchIcon} />
-      <input onChange={(e) => handleChange(e.target.value)} type="text" value={searchValue} placeholder={placeholder || ""}/>
-      <button className={styles.clearBtn} type="reset">
-        <SvgIcon iconName="xmark"/>
-      </button>
+      <input
+        onChange={(e) => handleChange(e.target.value)}
+        type="text"
+        value={searchValue}
+        placeholder={placeholder || ""}
+      />
+      {searchValue.length > 0 && (
+        <button
+          onClick={() => handleChange("")}
+          className={styles.clearBtn}
+          type="reset"
+        >
+          <SvgIcon iconName="xmark" />
+        </button>
+      )}
     </form>
   );
 };
